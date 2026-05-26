22e190979a428372fd810315d037b26e73bf941f
Gayeshi Jayasinghe <gayeshijayasinghe@gmail.com>
2026-05-27 07:58:08 +1000

    Initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from app import app
+
+def test_home():
+    tester = app.test_client()
+    response = tester.get('/')
+    assert response.status_code == 200
