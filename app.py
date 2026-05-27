fc055d6185ea66c1fea2aa29ec3f181c3091ed76
Gayeshi Jayasinghe <gayeshijayasinghe@gmail.com>
2026-05-27 21:29:52 +1000

    Fixed pylint issues

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
+"""Flask application for Student Wellness Predictor."""
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
+    """Home route."""
     return "Smart Student Wellness Predictor Running"
+
 
 @app.route('/health')
 def health():
+    """Health check route."""
     return "Application Healthy"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    
